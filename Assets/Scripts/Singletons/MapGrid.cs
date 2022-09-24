43f01fd07f6dbca9d3e167e0f937c2549ead570c
Alex <907795990@qq.com>
2022-09-24 21:21:15 +0800

    map grid

---
+++ Assets/Scripts/Singletons/MapGrid.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+public class MapGrid : MonoSingleton<MapGrid>
+{
+
+    [SerializeField]
+    private Grid grid;
+
+    public bool MouseRaycastAlignedPosition(Vector2 mousePosition, out Vector3 res)
+    {
+        res = Vector3.zero;
+
+        var layerMask = LayerMask.GetMask("MapGrid");
+        var ray = Camera.main.ScreenPointToRay(mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit hitInfo, 9999f, layerMask))
+        {
+            res = Align(grid, hitInfo.point);
+            return true;
+        }
+        return false;
+    }
+
+    private Vector3 Align(Grid grid, Vector3 position)
+    {
+        var offset = grid.CellToWorld(Vector3Int.one) * 0.5f; // offset to grid center
+        return grid.CellToWorld(grid.WorldToCell(position)) + offset;
+    }
+}
